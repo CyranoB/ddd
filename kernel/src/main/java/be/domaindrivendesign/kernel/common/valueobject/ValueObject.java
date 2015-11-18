4281948f5d00820aee092a81f6e91ecebb72811c
Eddie Pick <eddie@pick.fr>
2015-11-18 12:32:14 +0100

    ValueObject & tests

--- kernel/src/main/java/be/domaindrivendesign/kernel/common/valueobject/ValueObject.java
+++ kernel/src/main/java/be/domaindrivendesign/kernel/common/valueobject/ValueObject.java
@@ -1,8 +1,40 @@
 package be.domaindrivendesign.kernel.common.valueobject;
 
+import be.domaindrivendesign.kernel.rule.interfaces.RuleObject;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.reflect.FieldUtils;
+
+import java.lang.reflect.Field;
+
 /**
  * Created by eddie on 17/11/15.
  */
-public interface ValueObject<T> {
-    boolean isSameValueObjectAs(T otherObject);
+public class ValueObject implements RuleObject {
+    private transient int cachedHashCode = 0;
+
+    @Override
+    public final int hashCode() {
+        int h = cachedHashCode;
+        if (h == 0) {
+            h = HashCodeBuilder.reflectionHashCode(this, false);
+            cachedHashCode = h;
+        }
+        return h;
+    }
+
+    @Override
+    public final boolean equals(final Object o) {
+        return this == o || !(o == null || getClass() != o.getClass()) && EqualsBuilder.reflectionEquals(this, o, false);
+    }
+
+    public final boolean areEntirePropertyNull() throws IllegalAccessException {
+        final String clazz = this.getClass().getName();
+        for (Field f : this.getClass().getDeclaredFields()) {
+            if (!f.isSynthetic() && !clazz.equals(f.getName()) && FieldUtils.readField(f, this, true) != null) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
---
+++ kernel/src/test/java/be/domaindrivendesign/kernel/common/valueobject/ValueObjectTests.java
@@ -0,0 +1,101 @@
+package be.domaindrivendesign.kernel.common.valueobject;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by eddie on 17/11/15.
+ */
+
+public class ValueObjectTests {
+    @SuppressWarnings({"EqualsBetweenInconvertibleTypes", "EqualsWithItself", "ObjectEqualsNull"})
+    @Test
+    public void testValueObjectEquality() throws Exception {
+        MyValueObject01 v1a = new MyValueObject01(1, 4);
+        MyValueObject01 v1b = new MyValueObject01(1, 4);
+        MyValueObject01 v1c = new MyValueObject01(1, 5);
+        MyStringValueObject v2a = new MyStringValueObject("1", "4");
+        MyStringValueObject v2b = new MyStringValueObject("1", "4");
+
+        // Equals to itself
+        assertTrue(v1a.equals(v1a));
+        assertTrue(v2a.equals(v2a));
+
+        // Same type, same values
+        assertTrue(v1a.equals(v1b));
+        assertTrue(v2a.equals(v2b));
+
+        // Same type, different values
+        assertTrue(!v1a.equals(v1c));
+
+        // Different type, same values
+        assertTrue(!v1a.equals(v2a));
+
+        // null object
+        //noinspection ObjectEqualsNull
+        assertTrue(!v1a.equals(null));
+    }
+
+    @Test
+    public void testValueObjectEntirePropertiesAreNull() throws Exception {
+
+        MyValueObject01 vo1NotNull = new MyValueObject01(1, 2);
+        MyStringValueObject notNull = new MyStringValueObject("3", "4");
+        MyStringValueObject partiallyNull1 = new MyStringValueObject(null, "4");
+        MyStringValueObject partiallyNull2 = new MyStringValueObject("1", null);
+        MyMixedValueObject partiallyNull3 = new MyMixedValueObject(1, null);
+        MyMixedValueObject partiallyNull4 = new MyMixedValueObject(null, "toto");
+        MyStringValueObject allNull = new MyStringValueObject(null, null);
+        MyMixedValueObject mixedAllNull = new MyMixedValueObject(null, null);
+
+        // Not all values are null
+        assertTrue(!vo1NotNull.areEntirePropertyNull());
+        assertTrue(!notNull.areEntirePropertyNull());
+        assertTrue(!partiallyNull1.areEntirePropertyNull());
+        assertTrue(!partiallyNull2.areEntirePropertyNull());
+        assertTrue(!partiallyNull3.areEntirePropertyNull());
+        assertTrue(!partiallyNull4.areEntirePropertyNull());
+
+        // All values are null
+        assertTrue(allNull.areEntirePropertyNull());
+        assertTrue(mixedAllNull.areEntirePropertyNull());
+    }
+
+    @SuppressWarnings("unused")
+    final class MyValueObject01 extends ValueObject {
+
+        private final int value01;
+        private final int value02;
+
+        @SuppressWarnings("SameParameterValue")
+        public MyValueObject01(int value01, int value02) {
+            this.value01 = value01;
+            this.value02 = value02;
+        }
+    }
+
+    @SuppressWarnings("unused")
+    final class MyStringValueObject extends ValueObject {
+
+        private final String value1;
+        private final String value2;
+
+        public MyStringValueObject(String value1, String value2) {
+            this.value1 = value1;
+            this.value2 = value2;
+        }
+    }
+
+    @SuppressWarnings("unused")
+    final class MyMixedValueObject extends ValueObject {
+
+        private final Integer value1;
+        private final String value2;
+
+        public MyMixedValueObject(Integer value1, String value2) {
+            this.value1 = value1;
+            this.value2 = value2;
+        }
+    }
+}
