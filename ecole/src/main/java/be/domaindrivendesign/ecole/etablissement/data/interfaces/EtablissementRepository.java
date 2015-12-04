d03fad65d7c218cca456bd7950c422bbac8e403b
Eddie Pick <eddie@pick.fr>
2015-12-04 14:25:23 +0100

    JPA repositories

--- ecole/src/main/java/be/domaindrivendesign/ecole/etablissement/data/interfaces/EtablissementRepository.java
+++ ecole/src/main/java/be/domaindrivendesign/ecole/etablissement/data/interfaces/EtablissementRepository.java
@@ -2,7 +2,6 @@ package be.domaindrivendesign.ecole.etablissement.data.interfaces;
 
 import be.domaindrivendesign.ecole.etablissement.model.Etablissement;
 import be.domaindrivendesign.kernel.data.interfaces.Repository;
-import org.springframework.data.repository.CrudRepository;
 
 import java.util.UUID;
 
